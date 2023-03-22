e508423e7e6e90982fe979521dd991fe88a768df
JosephMumo <josephmumo25@gmail.com>
2023-03-22 14:27:11 +0000

    task-1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,12 @@
 export default function getResponseFromAPI(success) {
-	let success = true;
 	let myPromise = new Promise(function(resolve, reject) {
-		let success = true;
+		let object = {
+			status: 200,
+			body: 'Success'};
 		if (success === true) {
-			resolve(object {
-				status: 200,
-				body: 'Success'});
+			resolve(object);
 		} else {
 			reject("The fake API is not working currently");
 		}
-	}
-}
+	};
+};
